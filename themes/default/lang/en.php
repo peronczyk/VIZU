2553952db09bcec051cc6330b21b1c7f7f3f8dd0
Bartosz Perończyk <bartosz@peronczyk.com>
2017-04-05 09:16:39 +0200

    Allow to translate contact form system messages

--- themes/default/lang/en.php
+++ themes/default/lang/en.php
@@ -16,4 +16,15 @@
 	'page-not-found-title' => '404 Not Found',
 	'page-not-found-desc' => 'The page that you have requested could not be found.',
 	'page-not-found-link' => 'Home page',
+
+
+	# ==============================================================================
+	# Contact form
+	# ==============================================================================
+
+	'mailer-field-required'		=> 'This field is required',
+	'mailer-email-wrong'		=> 'Provided email address is not correct',
+	'mailer-flood'				=> '<strong>Message not sent</strong><br>You cannot send messages so often.',
+	'mailer-sent'				=> '<strong>Wiadomość została wysłana</strong><br>Dziękujemy za kontakt!<br />Postaramy się odpowiedzieć na nią jak najszybciej.',
+	'mailer-recipient-error'	=> '<strong>Message not sent</strong><br>There is no recipient configured.',
 );
